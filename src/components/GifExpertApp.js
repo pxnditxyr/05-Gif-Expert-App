0c77155a66d01bb62c8f9315e7fe3876ec4e5bd0
Pxndxs YR <pxnditxyr@gmail.com>
2021-04-19 01:45:01 -0400

    GifGrid

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { AddCategory } from './AddCategory';
+import { GifGrid } from './GifGrid';
+
+export const GifExpertApp = () => {
+
+	// const categories = [ "Naruto", "Bleach", "One Piece" ];
+	//const [ categories, setCategories ] = useState( ["Naruto", "Bleach", "One Piece", "KissxSis"] );
+	const [ categories, setCategories ] = useState( ["Naruto"] );
+
+
+	// const handleAdd = () => setCategories( [...categories, "KissxSis"] )
+
+	return (
+
+		<>
+			<h2> GifExpertApp XD </h2>
+			<AddCategory setCategories={ setCategories } />
+			<hr />
+			{/* <button onClick={ handleAdd }> Add </button> */}
+			<ol>
+				{
+					categories.map( category =>
+						//{/* <li key={ category }> { category } </li> */}
+						<GifGrid
+							key={ category }
+							category={ category }
+						/>
+					)
+				};
+			</ol>
+		</>
+	);
+};
---
+++ src/components/GifGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const GifGrid = ({ category }) => {
+	return (
+		<>
+			<h3> { category } </h3>
+		</>
+	);
+};
