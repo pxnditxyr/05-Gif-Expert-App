bbf4199873eb0dca9288cc4d7de73f359f8e72c0
Yuki xD <pxnditxyr@gmail..com>
2021-05-05 13:59:52 -0400

    Before of custom hooks

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,45 +1,40 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { GifGridItem } from './GifGridItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+// import { GifGridItem } from './GifGridItem';
+// import { getGifs } from '../helpers/getGifs';
 
 
 export const GifGrid = ({ category }) => {
 
-	const [ images, setImages ] = useState( [] );
-	useEffect( () => {
-		getImages();
-	}, [])
 
-	const getImages = async() => {
-
-		const url      = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=mlKQ969mBMhRH4cz0k3RRVsHGbrnWJ57`;
-		const response = await fetch( url );
-		const { data } = await response.json();
-		const gifs     = data.map( img => {
-			return ({
-				id: img.id,
-				title: img.title,
-				url: img.images?.downsized_medium.url,
-			});
-		});
-
-		setImages( gifs );
-	};
+	const { data, loading } = useFetchGifs();
+	console.log( data );
+	console.log( loading );
+	// const [ images, setImages ] = useState( [] );
+	// useEffect( () => {
+	// 	getGifs( category )
+	// 		.then( imgs => {
+	// 			setImages( imgs );
+	// 		});
+	// }, [ category ])
 
 
 	return (
 		<>
 			<h2> { category } </h2>
-			<div className="card-grid">
-				{
-					images.map( img => (
-						<GifGridItem
-							key={ img.id }
-							{ ...img  }
-						/>
-					))
-				}
-			</div>
+			{ loading ? 'Loading...' : 'Data charge done' }
+			{/* <div className="card-grid"> */}
+			{/* 	{ */}
+			{/* 		images.map( img => ( */}
+			{/* 			<GifGridItem */}
+			{/* 				key={ img.id } */}
+			{/* 				{ ...img  } */}
+			{/* 			/> */}
+			{/* 		)) */}
+			{/* 	} */}
+			{/* </div> */}
 		</>
 	);
 };
